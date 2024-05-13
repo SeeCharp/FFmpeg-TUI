3b68317d8154aa23fbe24ab10904e5631a9c1011
SeeCharp <zne32@protonmail.com>
2024-05-12 22:46:50 -0400

    Create basic interface for testing.

--- TUI/Program.cs
+++ TUI/Program.cs
@@ -1,3 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Terminal.Gui;
 
-Console.WriteLine("Hello, World!");
+namespace TUI;
+
+class Program {
+    private static void Main() {
+        Application.Init();
+        var top = Application.Top;
+
+        // Creates the top-level window to contain the UI
+        var win = new Window("FFmpeg TUI") {
+            X = 0,
+            Y = 1, // Leave one row for the top-level menu
+            Width = Dim.Fill(),
+            Height = Dim.Fill() - 1
+        };
+        top.Add(win);
+
+        // Add a menu
+        var menu = new MenuBar(new MenuBarItem[] {
+            new MenuBarItem("_File", new MenuItem[] {
+                new MenuItem("_Quit", "", () => { Application.RequestStop(); })
+            }),
+        });
+        top.Add(menu);
+
+        // Add some controls
+        win.Add(
+            new Label(3, 2, "Enter command options:"),
+            new TextField(25, 2, 40, ""),
+            new Button(3, 4, "Run"),
+            new ListView(new Rect(3, 6, 100, 4), new List<string> { "Output will be shown here..." })
+        );
+
+        Application.Run();
+    }
+}
